178938eb025388f5e76158c91a8a712b94517f71
vanessa hasckel <vanessahasckel00@hotmail.com>
2024-06-27 21:48:36 -0300

    fix: slider bar

--- src/utils/cardContents.jsx
+++ src/utils/cardContents.jsx
@@ -1,21 +1,36 @@
 export const cardContents = [
   {
-    title: "Mapa interativo",
+    title: "1. Mapa interativo",
     paragraph:
       "Encontre pontos de coleta próximos a você em um mapa intuitivo.",
   },
   {
-    title: "Cadastro de Pontos",
+    title: "2. Cadastro de Pontos",
     paragraph:
       "Adicione novos pontos de coleta e ajude a expandir a rede de reciclagem.",
   },
   {
-    title: "Informações Detalhadas",
+    title: "3. Informações Detalhadas",
     paragraph: "Veja quais materiais são aceitos em cada ponto de coleta.",
   },
   {
-    title: "Registro de Contribuições",
+    title: "4. Registro de Contribuições",
     paragraph:
       "Registre suas atividades de reciclagem e acompanhe seu impacto ambiental.",
+  },
+];
+
+export const userComments = [
+  {
+    name: "Mariana Costa",
+    text: "O que mais gosto no Recicla365 é a praticidade. Além de localizar os pontos de coleta, o app oferece informações detalhadas sobre o que pode ser reciclado e como. A interface é super intuitiva e amigável. Estou muito satisfeita!",
+  },
+  {
+    name: "Fernanda Mendes",
+    text: "O Recicla365 é um verdadeiro aliado na luta contra o desperdício. Através do app, descobri diversos pontos de coleta perto de casa e ainda pude aprender mais sobre a importância da reciclagem. Sinto que estou fazendo minha parte pelo meio ambiente.",
+  },
+  {
+    name: "Carlos Oliveira",
+    text: "Nunca foi tão fácil gerenciar meus resíduos. O Recicla365 me ajuda a separar corretamente os materiais recicláveis e ainda me mostra dicas valiosas sobre reciclagem. O impacto positivo no meio ambiente é visível e me sinto mais responsável com o futuro do nosso planeta.",
   },
 ];
