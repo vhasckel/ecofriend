f6d330455d38e1827fdd9b6926c8352abb5b7140
vanessa hasckel <vanessahasckel00@hotmail.com>
2024-08-25 20:07:00 -0300

    fix: corrigido formato de ondas do footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,18 +1,33 @@
 import { Box, Container, Typography, styled } from "@mui/material";
-import Paragraph from "../Paragraph/Paragraph";
+import CustomShapeDividerFooter from "../CustomShapeDividerFooter/CustomShapeDividerFooter";
+import Title from "../Title/Title";
 import theme from "../../theme/theme";
 
 const BoxStyled = styled(Box)(({ theme }) => ({
   backgroundColor: theme.palette.customColors.mediumGreen,
   flexDirection: "column",
-  padding: 20,
+  textAlign: "center",
+  paddingBottom: "50px",
+  paddingTop: "100px",
+  position: "relative",
+  overflow: "hidden",
+
+  [theme.breakpoints.up("sm")]: {
+    textAlign: "left",
+  },
 }));
 
 const Footer = () => {
   return (
     <BoxStyled component="footer">
-      <Container maxWidth="lg">
-        <Typography variant="subtitle1" color={theme.palette.customColors.text}>
+      <Container>
+        <CustomShapeDividerFooter />
+        <Typography
+          variant="subtitle1"
+          color={theme.palette.customColors.text}
+          lineHeight={1.2}
+          mb={3}
+        >
           Desenvolvido por vhasckel
         </Typography>
       </Container>
