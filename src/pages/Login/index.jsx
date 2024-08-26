18102c577735e32b708886c8b3e2736ddbaa302f
vanessa hasckel <vanessahasckel00@hotmail.com>
2024-08-25 21:53:54 -0300

    feat: estrutura do componente login

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,7 +1,49 @@
 import React from "react";
+import {
+  TextField,
+  Button,
+  Container,
+  Typography,
+  Paper,
+  Box,
+} from "@mui/material";
 
 const Login = () => {
-  return <div>olá</div>;
+  return (
+    <Container component="main" maxWidth="xs">
+      <Paper elevation={3} sx={{ padding: 3 }}>
+        <Typography variant="h5" align="center">
+          Entrar
+        </Typography>
+        <Box component="form" sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            fullWidth
+            id="email"
+            name="email"
+            label="Email"
+            type="email"
+          />
+          <TextField
+            margin="normal"
+            fullWidth
+            id="password"
+            name="password"
+            label="Senha"
+            type="password"
+          />
+          <Button
+            fullWidth
+            variant="contained"
+            color="primary"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Entrar
+          </Button>
+        </Box>
+      </Paper>
+    </Container>
+  );
 };
 
 export default Login;
